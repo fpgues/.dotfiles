eac27a47ddb032bc2852f5392119cf45a3857a99
fpgues <114113408+fpgues@users.noreply.github.com>
2025-03-23 22:07:43 -0300

    update

--- all/grub/install.sh
+++ all/grub/install.sh
@@ -1,11 +1,9 @@
 sudo mkdir -p /boot/grub/themes/
+
 #cp -r ~/.dotfiles/all/grub/archcraft/ /boot/grub/themes/
 
 sudo update-grub
 
 sudo reboot
 
-
-
-
-
+#copiar o arquivo grub para o arquivo em "sudo nano /etc/default/grub"
